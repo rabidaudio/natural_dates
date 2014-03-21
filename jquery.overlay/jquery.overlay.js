1f67b2d93e98edef9684e089dc68b779280ade74
Charles Julian Knight <charles@rabidaudio.com>
2014-03-21 13:10:49 -0400

    cleanup, README

--- jquery.overlay/jquery.overlay.js
+++ jquery.overlay/jquery.overlay.js
@@ -18,16 +18,20 @@ jQuery.overlay = (function($){
         var ww = $(window).width();
         $(ob).css('right', parseInt((ww-w)/2) + $(window).scrollLeft() );
     };
+    
+    if(!String.prototype.trimRight) String.prototype.trimRight = function(){ return this.replace(/\s+$/,"") };
         
     module.create = function(content, overbox_classes, overlay_classes){
         module.destroy();
+        overbox_classes = [""].concat(overbox_classes).join(' ').trimRight();    //it's almost like programming in ruby! :P
+        overlay_classes = [""].concat(overlay_classes).join(' ').trimRight();
         var ob = $('<div/>', {
-            id: 'ol-overbox',
-            class: "overbox"+(overbox_classes ? " "+overbox_classes : "")
+            id:     'ol-overbox',
+            class:  "overbox"+overbox_classes,
         }).appendTo(document.body);
         var ol = $('<div/>', {
-            id: 'ol-overlay',
-            class: "overlay"+(overlay_classes ? overlay_classes : "")
+            id:     'ol-overlay',
+            class:  "overlay"+overlay_classes,
         }).appendTo(document.body);
         $(ob).append(content);
         $(window).scroll(module.update);
