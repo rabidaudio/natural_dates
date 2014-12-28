1c5d19fa39c76e250443418b72f337e31c0d85ae
Charles Julian Knight <charles@rabidaudio.com>
2014-12-28 06:43:16 -0500

    working times with rounding passing tests

--- natural_dates/src/natural_dates.js
+++ natural_dates/src/natural_dates.js
@@ -11,30 +11,97 @@ Date = (function(D){
     var module = D.prototype;
 
     module.getNaturalTime = function(opts){
-        var hours = this.getHours();
-        var minutes = this.getMinutes();
-        var ampm = "am";
-        if( hours == 0 ){
-            hours = 12;
-        }else if(hours == 12){
-            ampm="pm";
-        }else if(hours>12){
-            hours = hours % 12;
-            ampm = "pm";
+        var d = this;
+        var round;
+        if(opts && opts.roundTime){
+            round = opts.roundTime % 60;
+        }else{
+            round = (D.natural.roundTime || 0) % 60;
         }
-        if( hours==12 && minutes==0 ) return (ampm=="am" ? "midnight" : "noon");
+        if(round > 0)
+            d.setMinutes(Math.round(d.getMinutes()/round)*round);
+        var hours = d.getHours();
+        var minutes = d.getMinutes();
 
-        // if(opts.round){
-        //     date.setMinutes( Math.round(date.getMinutes()/60*2)*30 ); //round to half-hours
+        var ampm = (hours >= 12 ? "pm" : "am");
+
+        var day_minutes = hours*60+minutes;
+        if(day_minutes >= (11.5*60) && day_minutes <= (12.5*60)){
+            //noon
+            var result = [];
+            if(minutes != 0){
+                if(hours==12){
+                    result.push(minutes);
+                    result.push(plural("minute",minutes));
+                    result.push("after");
+                }else{
+                    result.push(60-minutes);
+                    result.push(plural("minute",60-minutes));
+                    result.push("before");
+                }
+            }
+            result.push("noon");
+            return result.join(" ");
+        }
+        if(day_minutes >= (23.5*60) || day_minutes <= (1.5*60)){
+            //midnight
+            var result = [];
+            if(minutes != 0){
+                if(hours==0){
+                    result.push(minutes);
+                    result.push(plural("minute",minutes));
+                    result.push("past");
+                }else{
+                    result.push(60-minutes);
+                    result.push(plural("minute",60-minutes));
+                    result.push("to");
+                }
+            }
+            result.push("midnight");
+            return result.join(" ");
+        }
+
+        hours = (hours % 12)+"";
+
+        if(minutes==0)
+            return hours+" "+ampm;
+
+        minutes = minutes+"";
+        if(minutes.length < 2)
+            minutes="0"+minutes;
+
+        return hours+":"+minutes+" "+ampm;
+
+        // if(hours== 0 && minutes == 0){
+
         // }
-        return hours+( minutes != 0 ? ":"+(minutes<10?"0":"")+minutes : "" )+" "+ampm;
+
+        // if(hours == 0 && minutes<30){
+
+        // } (hours == 23 && minutes > 30)){
+        // }
+
+        // if( hours == 0 ){
+        //     hours = 12;
+        // }else if(hours == 12){
+        //     ampm="pm";
+        // }else if(hours>12){
+        //     hours = hours % 12;
+        //     ampm = "pm";
+        // }
+        // if( hours==12 && minutes==0 ) return (ampm=="am" ? "midnight" : "noon");
+
+        // // if(opts.round){
+        // //     date.setMinutes( Math.round(date.getMinutes()/60*2)*30 ); //round to half-hours
+        // // }
+        // return hours+( minutes != 0 ? ":"+(minutes<10?"0":"")+minutes : "" )+" "+ampm;
     }
 
     module.getNaturalDate = function(opts){
         var d = this;
         var refDate;
-        if(opts && opts.refDate)
-            refDate = opts.refDate;
+        if(opts && opts.referenceDate)
+            refDate = opts.referenceDate;
 
         if(within_days(d, -1, -1, refDate))
             return "Yesterday";
@@ -61,8 +128,8 @@ Date = (function(D){
     module.toNaturalString = function(opts){
         var d = this;
         var refDate;
-        if(opts && opts.refDate)
-            refDate = opts.refDate;
+        if(opts && opts.referenceDate)
+            refDate = opts.referenceDate;
 
         if(within_days(d,0,0, refDate))
             //do minutes ago/from now
@@ -72,10 +139,30 @@ Date = (function(D){
 
 
     //HELPERS
+    function minutes_diff(diff){
+        if(diff>=0 && diff<15)              return diff+plural(" minute", diff)+" from now";
+        else if(diff>=15  && diff < 55)     return Math.round(diff/5)*5 + plural(" minute", diff)+" from now";
+        else if(diff>=55  && diff<12*60)    return Math.round(diff/60)+plural(" hour", diff)+" from now";
+        else if(diff<=0   && diff>-15)      return -1*diff+plural(" minute", diff)+" ago";
+        else if(diff<=-15 && diff > -55)    return -1*Math.round(diff/5)*5 +plural(" hour", diff)+" ago";
+        else if(diff<=-55 && diff>-12*60)   return -1*Math.round(diff/60)+plural(" minute", diff)+" ago";
+        else                                return "Today at "+d.pretty_time();
+    }
+
+    var plural = function(word, num){
+        return word+(num==1 ? "" : "s");
+    }
+
     var within_days = function(d, start, finish, refDate){
         var today = refDate || Date.natural.referenceDate || new Date();
         var one_day = 1000*60*60*24;
         var diff = Math.ceil((d.getTime() - today.getTime())/one_day);
+        return (diff >= start && diff <= finish);
+    }
+    var within_minutes = function(d, start, finish, refDate){
+        var today = refDate || Date.natural.referenceDate || new Date();
+        var one_min = 1000*60;
+        var diff = Math.ceil((d - today)/one_min);
         return (diff >= start && diff <= finish);
     }
 
--- natural_dates/test/test.js
+++ natural_dates/test/test.js
@@ -4,59 +4,65 @@ var should = chai.should();
 
 require('../src/natural_dates');
 
-var REF_DATE = new Date('2015-01-01T06:00:00.000Z'); //a Thursday
+var REF_DATE = new Date('2015-01-01T06:30:00.000Z'); //a Thursday at 1:30 am EST
 Date.natural.referenceDate = REF_DATE;
 
 describe('Date', function(){
+
+  it('should be globally configurable', function(){
+    Date.natural.referenceDate.should.be.ok;
+  });
+
   describe('#getNaturalDate()', function(){
     it('should exist', function(){
-        new Date().should.have.property('getNaturalDate');
-        new Date().getNaturalDate.should.be.a.Function;
-    });
-
-    it('should be configurable', function(){
-        Date.natural.referenceDate.should.be.ok;
+      new Date().should.have.property('getNaturalDate');
+      new Date().getNaturalDate.should.be.a.Function;
     });
 
     it('should get Yesterday', function(){
-        var yesterday = new Date(2014, 11, 31);
-        yesterday.getNaturalDate().should.eql("Yesterday");
+      var yesterday = new Date(2014, 11, 31);
+      yesterday.getNaturalDate().should.eql("Yesterday");
     });
     it('should get Today', function(){
-        var today = new Date(2015, 0, 1);
-        today.getNaturalDate().should.eql("Today");
+      var today = new Date(2015, 0, 1);
+      today.getNaturalDate().should.eql("Today");
     });
     it('should get Tomorrow', function(){
-        var tomorrow = new Date(2015, 0, 2);
-        tomorrow.getNaturalDate().should.eql("Tomorrow");
+      var tomorrow = new Date(2015, 0, 2);
+      tomorrow.getNaturalDate().should.eql("Tomorrow");
     });
     it('should get later in the week', function(){
-        var monday = new Date(2015, 0, 5);
-        monday.getNaturalDate().should.eql("This Monday");
+      var monday = new Date(2015, 0, 5);
+      monday.getNaturalDate().should.eql("This Monday");
     });
     it('should get earlier in the week', function(){
-        var monday = new Date(2014, 11, 29);
-        monday.getNaturalDate().should.eql("Last Monday");
+      var monday = new Date(2014, 11, 29);
+      monday.getNaturalDate().should.eql("Last Monday");
     });
     it('should get two weeks from now', function(){
-        var monday = new Date(2015, 0, 12);
-        monday.getNaturalDate().should.eql("Next Monday the 12th");
+      var monday = new Date(2015, 0, 12);
+      monday.getNaturalDate().should.eql("Next Monday the 12th");
     });
     it('should get future dates this year',function(){
-        var april = new Date(2015, 3, 1);
-        april.getNaturalDate().should.eql('April 1st');
+      var april = new Date(2015, 3, 1);
+      april.getNaturalDate().should.eql('April 1st');
     });
     it('should get past dates this year',function(){
-        var april = new Date(2014, 9, 2);
-        april.getNaturalDate().should.eql('October 2nd');
+      var april = new Date(2014, 9, 2);
+      april.getNaturalDate().should.eql('October 2nd');
     });
     it('should get future dates other years',function(){
-        var april = new Date(2016, 3, 1);
-        april.getNaturalDate().should.eql('April 1st 2016');
+      var april = new Date(2016, 3, 1);
+      april.getNaturalDate().should.eql('April 1st 2016');
     });
     it('should get past dates other years',function(){
-        var april = new Date(2013, 9, 2);
-        april.getNaturalDate().should.eql('October 2nd 2013');
+      var april = new Date(2013, 9, 2);
+      april.getNaturalDate().should.eql('October 2nd 2013');
+    });
+
+    it('shold be instance configurable', function(){
+      var monday = new Date(2015, 0, 5);
+      monday.getNaturalDate({referenceDate: monday}).should.eql('Today');
     });
   });
 
@@ -70,21 +76,88 @@ describe('Date', function(){
   // });
 
   describe('#naturalTime()', function(){
-    it('should handle am', function(){
+    describe('exact times', function(){
+      it('should handle am', function(){
         var morning = new Date(2015, 0, 1, 3, 0);
         morning.getNaturalTime().should.eql('3 am');
-    });
-    it('should handle pm', function(){
+      });
+      it('should handle pm', function(){
         var afternoon = new Date(2015, 0, 1, 15, 0);
         afternoon.getNaturalTime().should.eql('3 pm');
-    });
-    it('should handle noon', function(){
+      });
+      it('should handle noon', function(){
         var noon = new Date(2015, 0, 1, 12, 0);
         noon.getNaturalTime().should.eql('noon');
-    });
-    it('should handle midnight', function(){
+      });
+      it('should handle midnight', function(){
         var midnight = new Date(2015, 0, 1, 0, 0);
         midnight.getNaturalTime().should.eql('midnight');
+      });
     });
+
+    describe('exact times with minutes', function(){
+      it('should handle am', function(){
+        var morning = new Date(2015, 0, 1, 3, 41);
+        morning.getNaturalTime().should.eql('3:41 am');
+      });
+      it('should handle pm', function(){
+        var afternoon = new Date(2015, 0, 1, 15, 41);
+        afternoon.getNaturalTime().should.eql('3:41 pm');
+      });
+      it('should handle before noon', function(){
+        var noon = new Date(2015, 0, 1, 11, 55);
+        noon.getNaturalTime().should.eql('5 minutes before noon');
+      });
+      it('should handle after noon', function(){
+        var noon = new Date(2015, 0, 1, 12, 4);
+        noon.getNaturalTime().should.eql('4 minutes after noon');
+      });
+      it('should handle before midnight', function(){
+        var midnight = new Date(2014, 11, 31, 23, 59);
+        midnight.getNaturalTime().should.eql('1 minute to midnight');
+      });
+      it('should handle after midnight', function(){
+        var midnight = new Date(2015, 0, 1, 0, 2);
+        midnight.getNaturalTime().should.eql('2 minutes past midnight');
+      });
+    });
+
+    describe('rounding', function(){
+      it('should round down to hour', function(){
+        var morning = new Date(2015, 0, 1, 3, 1);
+        morning.getNaturalTime({roundTime: 5}).should.eql('3 am');
+      });
+      it('should round up to hour', function(){
+        var morning = new Date(2015, 0, 1, 3, 53);
+        morning.getNaturalTime({roundTime: 15}).should.eql('4 am');
+      });
+      it('should round down to minute', function(){
+        var morning = new Date(2015, 0, 1, 3, 4);
+        morning.getNaturalTime({roundTime: 5}).should.eql('3:05 am');
+      });
+      it('should round up to minute', function(){
+        var morning = new Date(2015, 0, 1, 3, 50);
+        morning.getNaturalTime({roundTime: 15}).should.eql('3:45 am');
+      });
+
+
+      it('should round to noon', function(){
+        var noon = new Date(2015, 0, 1, 11, 56);
+        noon.getNaturalTime({roundTime: 15}).should.eql('noon');
+      });
+      it('should round near noon', function(){
+        var noon = new Date(2015, 0, 1, 12, 12);
+        noon.getNaturalTime({roundTime: 15}).should.eql('15 minutes after noon');
+      });
+      it('should round to midnight', function(){
+        var midnight = new Date(2014, 0, 1, 0, 6);
+        midnight.getNaturalTime({roundTime: 15}).should.eql('midnight');
+      });
+      it('should round near midnight', function(){
+        var midnight = new Date(2014, 11, 31, 23, 54);
+        midnight.getNaturalTime({roundTime: 5}).should.eql('5 minutes to midnight');
+      });
+    });
+
   });
 })
